9582a18ea52eba41e67fded26af82ebd0c5f0e0d
FaEsinoP <shaturnyyy@mail.ru>
2023-03-10 19:47:27 +0300

    Changing the database to MySQL

--- VayLares/VayLares/settings.py
+++ VayLares/VayLares/settings.py
@@ -82,8 +82,10 @@ WSGI_APPLICATION = 'VayLares.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'clothes_store',
+        'USER': 'root',
+        'PASSWORD': '5555555555A',
     }
 }
 
